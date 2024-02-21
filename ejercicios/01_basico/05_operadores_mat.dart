90a081bb0ab9f3f348e09b0f1616349341b0eb06
JuanPabloA1 <juanmeraagudelo@correounivalle.edu.co>
2024-02-21 00:02:33 -0500

    05_operadores_mat

---
+++ ejercicios/01_basico/05_operadores_mat.dart
@@ -0,0 +1,10 @@
+/**
+ * Un operador es un simbolo que le dice al compilador
+ * que debe de realizar una tarea
+ * matematica, relacional o logica
+ * y debe de producir un resultado
+ */
+
+main () {
+
+}
