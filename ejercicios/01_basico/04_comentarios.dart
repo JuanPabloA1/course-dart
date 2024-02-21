bc2fe9371b696b6dc019b5c449862315745c6f44
JuanPabloA1 <juanmeraagudelo@correounivalle.edu.co>
2024-02-20 23:55:30 -0500

    04_comentarios

---
+++ ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,22 @@
+/**
+ * Este archivo sirve para explicar los comentarios
+ * 
+ * [creadoEn] : Hoy
+ * 
+ */
+
+main () {
+
+  // Comentado a una sola linea
+  /**
+   * Comentado por bloques
+   */
+
+  /// Nombre: El nombre de la persona que quiero saludar
+  /// Mensaje: Es el saludo que quiero hacer
+
+  /// Funciona para saludar
+  /// Recibe un [nombre] y se concatena con el [mensaje]
+  saludar (String nombre, String mensaje) {}
+
+}
