536dceef3d6b79bb551e90d0cfd18105d2e70add
JuanPabloA1 <juanmeraagudelo@correounivalle.edu.co>
2024-02-21 16:32:47 -0500

    operadores_asignacion

--- ejercicios/01_basico/06_operadores_asig.dart
+++ ejercicios/01_basico/06_operadores_asig.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 main () {
 
   // Operadores de asignacion
@@ -9,4 +11,16 @@ main () {
   int d = b ?? a; // Indica que si b es nulo asigne el valor de a
 
   print(b);
+
+  // Operadores relacionales
+  // Todos retornan un valor booleano
+
+  int i = 10;
+  String j = '10';
+
+  print( i is int ); // Sirven para ejecutar tipos en tiempo de ejecucion 
+  print( j is int );
+  
+  print( i is! int);
+
 }
