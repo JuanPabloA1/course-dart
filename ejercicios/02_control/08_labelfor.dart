0b19150fd9927a2e198700698e7c14dc1b818f87
JuanPabloA1 <juanmeraagudelo@correounivalle.edu.co>
2024-02-21 17:37:00 -0500

    08_labelfor

---
+++ ejercicios/02_control/08_labelfor.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+
+  /**
+   * El labelfor nos permite ponerle sobre nombre a nuestros ciclos
+   * esto nos permite por medio del break o el continue romper o continuar la secuencia
+   * con nombre propio.
+   */
+  
+  outerLoop:
+  for (int i=0; i<5; i++) {
+    print('Valor de i: $i');
+    
+    innerLoop:
+    for (int j=0; j<5; j++) {
+      print('Valor de j: $j');
+      
+      if ( j == 1) {
+        break outerLoop;
+      }
+    }
+  }
+}
