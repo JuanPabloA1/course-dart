92f22686978fa7cca1d1ea79cc3e52cce8c93e13
JuanPabloA1 <juanmeraagudelo@correounivalle.edu.co>
2024-02-22 17:16:25 -0500

    06_operadores_asig

--- ejercicios/01_basico/06_operadores_asig.dart
+++ ejercicios/01_basico/06_operadores_asig.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 main () {
 
   // Operadores de asignacion
@@ -10,7 +8,9 @@ main () {
   b ??= 20; // Asigna el valor unicamente si la variable es null
   int d = b ?? a; // Indica que si b es nulo asigne el valor de a
 
+  print(a);
   print(b);
+  print(d);
 
   // Operadores relacionales
   // Todos retornan un valor booleano
@@ -18,9 +18,11 @@ main () {
   int i = 10;
   String j = '10';
 
+  // ignore: unnecessary_type_check
   print( i is int ); // Sirven para ejecutar tipos en tiempo de ejecucion 
   print( j is int );
   
+  // ignore: unnecessary_type_check
   print( i is! int);
 
 }
