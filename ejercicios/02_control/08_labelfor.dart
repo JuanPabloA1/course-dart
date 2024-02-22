5909c9522a4f457981095a6fc0f8b274a1bb3964
JuanPabloA1 <juanmeraagudelo@correounivalle.edu.co>
2024-02-22 17:26:23 -0500

    08_labelfor

--- ejercicios/02_control/08_labelfor.dart
+++ ejercicios/02_control/08_labelfor.dart
@@ -10,6 +10,7 @@ void main(List<String> args) {
   for (int i=0; i<5; i++) {
     print('Valor de i: $i');
     
+    // ignore: unused_label
     innerLoop:
     for (int j=0; j<5; j++) {
       print('Valor de j: $j');
